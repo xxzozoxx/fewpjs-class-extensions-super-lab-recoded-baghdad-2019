27afa7c15cdde16aed73aba2c880315d4a8f96b7
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-10-16 20:06:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ class Evergreen extends Tree{
     this.name = name
   }
   static definition(){
-    return super.definition() +''
+    return super.definition() +'Evergreens keep their leaves all year round.'
   }
 }
